3b6cecf3be987e6d67e4f1b577b974f35b947014
isa-iaap <alvespeixotoisabela@gmail.com>
2026-04-19 22:38:39 -0300

    Add guest registration functionality in main.cpp
    Implement initial guest registration system.

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string nome;
+    
+    cout << "Sistema de Hospedagem SETECEB\n";
+    cout << "Digite o nome do hospede: ";
+    cin >> nome;
+
+    cout << "Hospede " << nome << " cadastrado com sucesso!\n";
+
+    return 0;
+}
+//Implementação inicial do cadastro de hóspedes
